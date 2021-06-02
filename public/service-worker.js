c09fe53279706e5e227d146638f26b3274beca83
Janeé Mays <janee.elise@gmail.com>
2021-06-02 17:06:23 -0400

    files_to_cache in service-worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,1 +1,7 @@
-console.log("Hi from your service-worker.js file!");
+const FILES_TO_CACHE = [
+    '/',
+    '/public/index.html',
+    '/public/style.css',
+    'https://fonts.googleapis.com/css?family=Istok+Web|Montserrat:800&display=swap',
+    'https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css',
+  ];
